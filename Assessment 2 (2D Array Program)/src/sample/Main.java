e28fcef3a3ed6de5b8a3e4e38f0c9a00440f55fc
gwilmott012 <J275834@tafe.wa.edu.au>
2018-04-23 15:21:35 +0800

    Commenting
    Adding comments to methods.

--- Assessment 2 (2D Array Program)/src/sample/Main.java
+++ Assessment 2 (2D Array Program)/src/sample/Main.java
@@ -19,6 +19,7 @@ import java.io.*;
 import static java.lang.System.out;
 
 public class Main extends Application {
+	// Create all private class variables
     private Button viewSeats, newBooking, saveBooking, cancelNewBooking, showCancelBooking, cancelBooking, searchBooking, back;
     private ComboBox adultOrChild, classType, seatType;
     private TextField name, cancelBookingName;
@@ -28,59 +29,22 @@ public class Main extends Application {
     private Text seats, _name, _adultOrChild, _classType, _seatType, message, _cancelBookingName, _reservation, _reservationNumber;
     private SeatingArray seatingArray = new SeatingArray();
 
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         try {
             window = primaryStage;
+            // Set the title of the program
             window.setTitle("2D Array Program");
 
-            out.println(seatingArray.AllocateSeat("George", PersonType.Adult, ClassType.First, SeatType.Window));
-            out.println(seatingArray.AllocateSeat("Alan", PersonType.Adult, ClassType.First, SeatType.Window));
-            out.println(seatingArray.AllocateSeat("Esther", PersonType.Adult, ClassType.First, SeatType.Window));
-            out.println(seatingArray.AllocateSeat("Nicola", PersonType.Adult, ClassType.First, SeatType.Window));
-            out.println(seatingArray.AllocateSeat("Jill", PersonType.Adult, ClassType.First, SeatType.Window));
-            out.println(seatingArray.AllocateSeat("Harry Potter", PersonType.Child, ClassType.Business, SeatType.Aisle));
-
-            CustomerArray ca =  CustomerArray.getInstance();
+            // Allocate temp data to seats
+            seatingArray.AllocateSeat("George", PersonType.Adult, ClassType.First, SeatType.Window);
+            seatingArray.AllocateSeat("Alan", PersonType.Adult, ClassType.First, SeatType.Window);
+            seatingArray.AllocateSeat("Esther", PersonType.Adult, ClassType.First, SeatType.Window);
+            seatingArray.AllocateSeat("Nicola", PersonType.Adult, ClassType.First, SeatType.Window);
+            seatingArray.AllocateSeat("Jill", PersonType.Adult, ClassType.First, SeatType.Window);
+            seatingArray.AllocateSeat("Harry Potter", PersonType.Child, ClassType.Business, SeatType.Aisle);    
             
-            
-            // Instantiating grids and buttons
+            // Instantiating grids, buttons, etc.
             gridMain = new GridPane();
             gridNew = new GridPane();
             gridView = new GridPane();
@@ -93,7 +57,7 @@ public class Main extends Application {
             searchBooking = new Button("Search");
             showCancelBooking = new Button("Cancel Booking");
             back = new Button("Back");
-            
+
             name = new TextField();
             cancelBookingName = new TextField();
             adultOrChild = new ComboBox(FXCollections.observableArrayList(PersonType.values()));
@@ -163,17 +127,15 @@ public class Main extends Application {
             // adding children to grids
             gridMain.getChildren().addAll(newBooking, viewSeats, showCancelBooking);
             gridNew.getChildren().addAll(saveBooking, cancelNewBooking, _name, name, _adultOrChild, adultOrChild, _seatType, seatType, _classType, classType);
-            
             gridCancel.getChildren().addAll(searchBooking, cancelBookingName, _cancelBookingName, back);
-            //gridNew.setAlignment(Pos.BOTTOM_LEFT);
 
             //Set Scenes
             main = new Scene(gridMain, 600, 460);
             _newBooking = new Scene(gridNew, 475, 460);
             _cancelBooking = new Scene(gridCancel, 475, 460);
 
+            // Set Scene to window and show window
             window.setScene(main);
-            
             window.show();
         } catch (IllegalArgumentException ex) {
             ex.getMessage();
@@ -182,6 +144,7 @@ public class Main extends Application {
         }
     }
 
+    // Sets the Width of new booking elements
     private void SetWidth() {
         name.setMinWidth(100);
         adultOrChild.setMinWidth(100);
@@ -193,10 +156,33 @@ public class Main extends Application {
         cancelNewBooking.setMinHeight(30);
     }
     
+    // Views allocated seats on main window
+    private void ViewSeats() {
+        seats.setText(seatingArray.toString());
+        seats.setFont(Font.font("Monospaced", 27));
+
+        if (!gridMain.getChildren().contains(seats))
+        {
+            GridPane.setConstraints(seats, 0, 3);
+            gridMain.getChildren().add(seats);
+        }
+    }
+
+    // Sets the scene of the window to the main screen
     private void BackToMain() {
     	window.setScene(main);
     }
 
+    // Brings up the new booking window
+    private void NewBooking() {
+        name.setText("");
+        adultOrChild.getSelectionModel().clearSelection();
+        seatType.getSelectionModel().clearSelection();
+        classType.getSelectionModel().clearSelection();
+        window.setScene(_newBooking);
+    }
+
+    // Allocates seat to customer and writes to text file
     private void SaveBooking() {
         String _AdultOrChild = adultOrChild.getValue().toString();
         String _SeatType = seatType.getValue().toString();
@@ -232,6 +218,7 @@ public class Main extends Application {
         }
     }
 
+    // Cancels the new booking and sets scene to main screen
     private void CancelNewBooking() {
         window.setScene(main);
         if (gridMain.getChildren().contains(seats)) {
@@ -240,6 +227,7 @@ public class Main extends Application {
 
     }
 
+    // Brings up the cancel booking window
     private void ShowCancelBooking() {
     	window.setScene(_cancelBooking);
     	
@@ -251,6 +239,7 @@ public class Main extends Application {
 		}
     }
     
+    // Searches bookings for certain customer and shows the cancel button in cancel booking
     private void SearchBooking() {
     	String name = cancelBookingName.getText();;
     	String column = "";
@@ -293,6 +282,7 @@ public class Main extends Application {
     	}
     }
     
+    // Cancels a booking for searched customer
     private void CancelBooking() {
     	String name = cancelBookingName.getText();
     	Customer customer = CustomerArray.getInstance().SearchCustomer(name);
@@ -316,53 +306,6 @@ public class Main extends Application {
     	
     	window.setScene(main);
     }   	
-
-
-    private void NewBooking() {
-        name.setText("");
-        adultOrChild.getSelectionModel().clearSelection();
-        seatType.getSelectionModel().clearSelection();
-        classType.getSelectionModel().clearSelection();
-        window.setScene(_newBooking);
-    }
-
-    private void ViewSeats() {
-        seats.setText(seatingArray.toString());
-        seats.setFont(Font.font("Monospaced", 27));
-
-        if (!gridMain.getChildren().contains(seats))
-        {
-            GridPane.setConstraints(seats, 0, 3);
-            gridMain.getChildren().add(seats);
-        }
-    }
-
-    @SuppressWarnings("unused")
-	private boolean isInt(TextField input, String message){
-        try {
-            int age = Integer.parseInt(message);
-            out.println("User is: " + age);
-            return true;
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-            out.println("Error: " + message + " is not a number");
-            return false;
-        }
-    }
-
-    @SuppressWarnings("unused")
-	private boolean isAlphabet(String input){
-        String[] strings = input.split("");
-
-        for (int i = 0; i < strings.length; i++) {
-            for(char letter = 'A'; letter <= 26; letter++){
-                if(strings[i].equalsIgnoreCase(""+letter)){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
 
     public static void main(String[] args) {
         launch(args);
